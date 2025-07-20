35b586f1b660aa276706908af6da8b40a3f2ba60
samrathD <samrath2004@gmail.com>
2025-07-19 22:05:34 -0700

    Set-up database

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,10 @@
 export const signup = (req, res) => {
+  const {email, fullName, password} = req.body;
+  try{
+    
+  }catch(err){
+
+  }
   res.send("signup route");
 };
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(express.json());
 
 app.use("/api/auth", authRoutes)
 app.get("/",(req,res)=>{
     res.send(`Welcome to home page ${req.query.name}`)    
 })
-app.listen(5001, ()=>{
-    console.log("server is running on port 5001")
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+    connectDB();
 })
